8af6efe2ce9b20727cd02ff4919a7bd99bbd0295
sectionr0 <sectionr0@gmail.com>
2021-10-18 03:23:30 +0900

    add SplashActivity & JoinActivity
    로그인 구현은 아직 오류가 있어서 해결 중 주석 처리로 해 놓음

---
+++ app/src/main/java/com/team1/volunteerapp/JoinActivity.kt
@@ -0,0 +1,46 @@
+package com.team1.volunteerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        val email = findViewById<EditText>(R.id.emailEditArea)
+        val password = findViewById<EditText>(R.id.passwordEditArea)
+
+
+
+
+//            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+//                .addOnCompleteListener(this) { task ->
+//                    if (task.isSuccessful) {
+//                        // Sign in success, update UI with the signed-in user's information
+//                        Toast.makeText(this,"회원가입 성공", Toast.LENGTH_SHORT).show()
+//                        val Intent = Intent(this, HomeActivity::class.java)
+//                        startActivity(Intent)
+//                    } else {
+//                        // If sign in fails, display a message to the user.
+//                        Log.w("TAG", "createUserWithEmail:failure", task.exception)
+//                        Toast.makeText(this,"회원가입 실패",Toast.LENGTH_SHORT).show()
+//
+//                    }
+//                }
+
+        }
+    }
