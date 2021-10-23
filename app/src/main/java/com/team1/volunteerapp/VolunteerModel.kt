b2f832bae875510e07d3384ca9c2ef6749026d5d
sectionr0 <sectionr0@gmail.com>
2021-10-23 18:59:04 +0900

    add dataModel
    Volunteer data model 추가
    
    API 데이터 항목 이름이랑 같게 설정함

---
+++ app/src/main/java/com/team1/volunteerapp/VolunteerModel.kt
@@ -0,0 +1,8 @@
+package com.team1.volunteerapp
+
+data class VolunteerModel(
+    val nanmmbyNm: String,
+    val progrmSj: String = "",
+    val progrmBgnde: String = "",
+    val progrmEndde: String = ""
+)
