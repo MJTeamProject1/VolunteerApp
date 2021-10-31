504eaafa9bbeb0995e806fd2201869fb05dfea4f
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2021-11-01 01:00:25 +0900

    update profile
    프로필 업데이트입니다.

--- app/src/main/java/com/team1/volunteerapp/ProfileActivity.kt
+++ app/src/main/java/com/team1/volunteerapp/ProfileActivity.kt
@@ -1,14 +1,35 @@
 package com.team1.volunteerapp
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var pieChart: PieChart
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val homeButton = findViewById<ImageButton>(R.id.homeButton)
+
+
+        pieChart = findViewById(R.id.PieChartMyVolune)
+
+        initPieChart()
+
+        setDataToPieChart()
 
         // RecyclerView 임시 테스트 데이터 삽입
         val items = mutableListOf<String>()
@@ -34,6 +55,66 @@ class ProfileActivity : AppCompatActivity() {
 //            }
 //
 //        }
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+   //Pie Chart 생성
+    private fun initPieChart() {
+        pieChart.setUsePercentValues(true)
+        pieChart.description.text = "봉사시간 현황"
+        //hollow pie chart
+        pieChart.isDrawHoleEnabled = false
+        pieChart.setTouchEnabled(false)
+        pieChart.setDrawEntryLabels(false)
+        //adding padding
+        pieChart.isRotationEnabled = false
+        pieChart.legend.orientation = Legend.LegendOrientation.VERTICAL
+        pieChart.legend.isWordWrapEnabled = true
+
+    }
+
+    private fun setDataToPieChart() {
+        pieChart.setUsePercentValues(true)
+        val dataEntries = ArrayList<PieEntry>()
+        dataEntries.add(PieEntry(100f, "봉사시간"))
+        dataEntries.add(PieEntry(5f, "남은시간")) // 입력된 목표 봉사시간에서 -하기
+
+        val colors: ArrayList<Int> = ArrayList() // 각 영역의 색상
+        colors.add(Color.parseColor("#4DD0E1"))
+        colors.add(Color.parseColor("#FFF176"))
+
+        val dataSet = PieDataSet(dataEntries, "")
+        val data = PieData(dataSet)
+
+        // %로 설정
+        data.setValueFormatter(PercentFormatter())
+        dataSet.sliceSpace = 3f
+        dataSet.colors = colors
+        pieChart.data = data
+        data.setValueTextSize(15f)
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+        //가운데 빈 공간
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setHoleColor(Color.WHITE)
+
+
+        //빈 공간에 텍스트
+        pieChart.setDrawCenterText(true);
+        pieChart.centerText = "봉사시간 : XXX시간"
+
+
+
+        pieChart.invalidate()
 
     }
 
