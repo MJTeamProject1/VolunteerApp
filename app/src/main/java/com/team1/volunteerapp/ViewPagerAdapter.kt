6f2fd3b9dd9fedfaac13714c6c7c95d8cfd020ab
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2021-11-03 22:13:40 +0900

    update banner
    배너 업데이트했습니다.

--- app/src/main/java/com/team1/volunteerapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/team1/volunteerapp/ViewPagerAdapter.kt
@@ -1,8 +1,17 @@
 package com.team1.volunteerapp
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class ViewPagerAdapter(bannerList: ArrayList<Int>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
@@ -10,15 +19,40 @@ class ViewPagerAdapter(bannerList: ArrayList<Int>) : RecyclerView.Adapter<ViewPa
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
 
-    override fun getItemCount(): Int = 2	// 아이템의 갯수를 임의로 확 늘린다.
+    override fun getItemCount(): Int = 2
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.banner.setImageResource(item[position])	// position에 3을 나눈 나머지 값을 사용한다.
+        holder.banner.setImageResource(item[position])
     }
 
+    // itemClick 인터페이스
+    interface ItemClick{
+        fun onClick(view : View, position: Int)
+    }
+    var itemClick : ItemClick? = null
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
         (LayoutInflater.from(parent.context).inflate(R.layout.banner_list_item, parent, false)) {
 
         val banner = itemView.findViewById<ImageView>(R.id.imageView_banner)!!
+        init {
+            itemView.setOnClickListener {
+
+                if(position == 0){
+                    //1365 홈페이지 접속
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.1365.go.kr/vols/main.do"))
+                    parent.context.startActivity(intent)
+                }
+
+                else if(position == 1){
+                    //개발자 이메일 보내기
+                    var emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "rudtjr1206@naver.com", null))
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "")
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, "")
+                    parent.context.startActivity(Intent.createChooser(emailIntent, ""))
+                }
+            }
+        }
     }
 }
