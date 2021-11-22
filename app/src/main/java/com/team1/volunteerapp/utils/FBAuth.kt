57a6d4d44f8bf1420fea15b5b3e0c0fce204f7d0
sectionr0 <sectionr0@gmail.com>
2021-11-23 07:01:01 +0900

    Update FBAuth
    이제 유저 정보를 쉽게 불러올 수 있음

--- app/src/main/java/com/team1/volunteerapp/utils/FBAuth.kt
+++ app/src/main/java/com/team1/volunteerapp/utils/FBAuth.kt
@@ -1,11 +1,26 @@
 package com.team1.volunteerapp.utils
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
 
 class FBAuth {
     companion object{
+        private val db = FirebaseFirestore.getInstance()
+        private var vol_time: String? = null
+        private var vol_title: String? = null
+        private var vol_goaltime: String? = null
+        private var vol_user: String? = null
+        private var user_email: String? = null
+        private var vol_name: String? = null
+        private var user_sido: String? = null
+        private var user_gugun: String? = null
+        private var user_phone: String? = null
+
+
         private lateinit var auth: FirebaseAuth
 
         fun getUid() : String{
@@ -18,6 +33,59 @@ class FBAuth {
             val dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(currentTime)
 
             return dateFormat
+        }
+
+        fun getUserData(num : Int) : String? {
+            auth = Firebase.auth
+            var result = ""
+            db.collection("UserData")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (doc in result) {
+                        if (doc["uid"] == auth.uid.toString()) {
+
+                            if (doc["vol_time"] != null) {
+                                vol_time = doc["vol_time"].toString()
+                            }
+                            if (doc["vol_title"] != null) {
+                                vol_title = doc["vol_title"].toString()
+                            }
+                            if (doc["timedata"] != null) {
+                                vol_goaltime = doc["timedata"].toString()
+                            }
+                            if (doc["nickname"] != null) {
+                                vol_user = doc["nickname"].toString()
+                            }
+                            if (doc["email"] != null) {
+                                user_email = doc["email"].toString()
+                            }
+                            if (doc["username"] != null) {
+                                vol_name = doc["username"].toString()
+                            }
+                            if (doc["sidodata"] != null) {
+                                user_sido = doc["sidodata"].toString()
+                            }
+                            if (doc["gugundata"] != null) {
+                                user_gugun = doc["gugundata"].toString()
+                            }
+                            if (doc["phonenumber"] != null) {
+                                user_phone = doc["phonenumber"].toString()
+                            }
+                        }
+                    }
+                }
+            when(num){
+                1-> result = vol_time.toString()
+                2-> result = vol_title.toString()
+                3-> result = vol_goaltime.toString()
+                4-> result = vol_user.toString()
+                5-> result = user_email.toString()
+                6-> result = vol_name.toString()
+                7-> result = user_sido.toString()
+                8-> result = user_gugun.toString()
+                9-> result = user_phone.toString()
+            }
+            return result
         }
     }
 }
