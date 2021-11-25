9e81c08f4da3d9f3de101ba891d9b4d8e38155e2
sectionr0 <sectionr0@gmail.com>
2021-11-25 16:32:40 +0900

    update chatRoom
    자신이 쓴 글인 경우 노란색으로 표시

--- app/src/main/java/com/team1/volunteerapp/Chat/ChatInsideRVAdater.kt
+++ app/src/main/java/com/team1/volunteerapp/Chat/ChatInsideRVAdater.kt
@@ -7,28 +7,34 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.team1.volunteerapp.R
+import com.team1.volunteerapp.utils.FBAuth
 
 class ChatInsideRVAdater (val items : MutableList<ChatModel>): RecyclerView.Adapter<ChatInsideRVAdater.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatInsideRVAdater.ViewHolder {
-        val view1 = LayoutInflater.from(parent.context).inflate(R.layout.chat_rv_item,parent,false)
+        when(viewType){
+            0 -> {
+                val view1 = LayoutInflater.from(parent.context).inflate(R.layout.chat_my_rv_item,parent,false)
+                return ViewHolder(view1)
+            }
+            else ->{
+                val view1 = LayoutInflater.from(parent.context).inflate(R.layout.chat_rv_item,parent,false)
+                return ViewHolder(view1)
+            }
+        }
 
-        return ViewHolder(view1)
     }
 
     override fun getItemViewType(position: Int): Int {
-//        items.
-//        val chatMessage: ChatMessage = items[position]
-//        Log.d("FFFFFF", "겟아이템뷰타입 : " + chatMessage.getuId())
-//        Log.d("FFFFFF", "겟아이템뷰타입 내 토큰 : $mProfileUid")
-//        return if (chatMessage.getuId().equals(mProfileUid)) { // 내 아이디인 경우 오른쪽뷰로 분기 (0)
-//            0
-//        } else { // 왼쪽뷰 (1)
-//            1
-//        }
-        return super.getItemViewType(position)
-    }
-    override fun onBindViewHolder(holder: ChatInsideRVAdater.ViewHolder, position: Int) {
+        return if(items[position].uid == FBAuth.getUid()){
+            0
+        }else{
+            1
+        }
 
+//        return super.getItemViewType(position)
+    }
+
+    override fun onBindViewHolder(holder: ChatInsideRVAdater.ViewHolder, position: Int) {
         holder.bindItems(items[position])
     }
 
@@ -38,6 +44,7 @@ class ChatInsideRVAdater (val items : MutableList<ChatModel>): RecyclerView.Adap
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private val chatText : TextView = itemView.findViewById(R.id.chatRvText)
+
         fun bindItems(item :ChatModel){
             chatText.text = item.text
         }
