0d596bc2749b44c84323adbb0cf18d12047f8c9d
sectionr0 <sectionr0@gmail.com>
2021-12-09 11:24:27 +0900

    Update LoginActivity
    로그인버튼 애니메이션 적용

--- app/src/main/java/com/team1/volunteerapp/Auth/LoginActivity.kt
+++ app/src/main/java/com/team1/volunteerapp/Auth/LoginActivity.kt
@@ -3,16 +3,20 @@ package com.team1.volunteerapp.Auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.team1.volunteerapp.BtnLoadingProgressbar
 import com.team1.volunteerapp.R
 import com.team1.volunteerapp.Loading.SplashActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    val handler = Handler()
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,43 +26,57 @@ class LoginActivity : AppCompatActivity() {
 
         val email = findViewById<EditText>(R.id.emailEditArea_login)
         val password = findViewById<EditText>(R.id.passwordEditArea_login)
-        val loginbtn = findViewById<Button>(R.id.loginBtn)
 
-        loginbtn.setOnClickListener {
+        activity_login_btn.setOnClickListener {
+            val progressbar = BtnLoadingProgressbar(it)
             val inputemail = email.text.toString()
             val inputpassword = password.text.toString()
             var isGoToJoin = true
 
-            if(inputemail.isEmpty()){
-                Toast.makeText(this,"이메일을 입력하지 않았습니다",Toast.LENGTH_LONG).show()
+            if (inputemail.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력하지 않았습니다", Toast.LENGTH_LONG).show()
                 isGoToJoin = false
-            }
-
-            else if(inputpassword.isEmpty()){
-                Toast.makeText(this,"비밀번호를 입력하지 않았습니다",Toast.LENGTH_LONG).show()
+            } else if (inputpassword.isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력하지 않았습니다", Toast.LENGTH_LONG).show()
                 isGoToJoin = false
-            }
-            else {
+            } else {
                 if (isGoToJoin) {
                     auth.signInWithEmailAndPassword(inputemail, inputpassword)
                         .addOnCompleteListener(this) { task ->
                             if (task.isSuccessful) {
-                                // Sign in success, update UI with the signed-in user's information
+                                progressbar.setLoading()
+                                handler.postDelayed({
+                                    progressbar.setState(true){
+                                        handler.postDelayed({
+                                            val intent = Intent(this, SplashActivity::class.java)
+                                            finishAffinity()
+                                            startActivity(intent)
+                                            finish()
 
-                                val intent = Intent(this, SplashActivity::class.java)
-                                // 기존 엑티비티를 다 날림
-                                finishAffinity()
-                                startActivity(intent)
-                                finish()
+                                        }, 1500)
+                                    }
+                                }, 2000)
 
                             } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                                startError(progressbar)
                             }
                         }
                 }
+
             }
         }
-
+    }
+    private fun startError(progressbar: BtnLoadingProgressbar) {
+        progressbar.reset()
+        handler.postDelayed({
+            progressbar.setLoading()
+            handler.postDelayed({
+                progressbar.setState(false) {
+                    handler.postDelayed({
+                        progressbar.reset()
+                    }, 1500)
+                }
+            }, 2000)
+        }, 600)
     }
 }
